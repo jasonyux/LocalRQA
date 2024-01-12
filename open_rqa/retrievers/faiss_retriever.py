5514af116b74d8ff397837e5e4330781920f94ad
Xiao Yu <jasonyux17@gmail.com>
2024-01-12 20:51:31 +0000

    fixed retriever using LangChain docs and returning LangChain docs

--- open_rqa/retrievers/faiss_retriever.py
+++ open_rqa/retrievers/faiss_retriever.py
@@ -5,16 +5,9 @@ from langchain.storage import (
 )
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.schema.document import Document as LangChainDocument
 from open_rqa.schema.document import Document
 from open_rqa.retrievers.base import BaseRetriever, RetrievalOutput
-
-
-def langchain_doc_to_open_rqa_doc(langchain_doc: LangChainDocument):
-    return Document.from_dict({
-        'page_content': langchain_doc.page_content,
-        'metadata': langchain_doc.metadata
-    })
+from copy import deepcopy
 
 
 class FaissRetriever(BaseRetriever):
@@ -36,9 +29,30 @@ class FaissRetriever(BaseRetriever):
         cached_embedder = CacheBackedEmbeddings.from_bytes_store(
             embeddings, fs, namespace="customized"
         )
-        super().__init__(texts, cached_embedder)
+        docs = self.prepare_docs_for_retrieval(texts)
+
+        super().__init__(docs, cached_embedder)
         self.retriever = self._init_retriever()
         return
+
+    def prepare_docs_for_retrieval(self, texts: List[Document]):
+        """prepare documents so that LangChain FAISS would recognize it, and embed using our fmt_content
+
+        Args:
+            texts (List[Document]): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        langhchain_docs = []
+        for doc in texts:
+            l_doc = doc.to_langchain_doc()
+            # make sure we can recover this later
+            l_doc.metadata['page_content'] = doc.page_content
+            l_doc.metadata['fmt_content'] = doc.fmt_content
+            l_doc.page_content = doc.fmt_content  # page_content is embedded
+            langhchain_docs.append(l_doc)
+        return langhchain_docs
     
     def _init_retriever(self, **kwargs):
         """initiate FAISS retriever
@@ -46,7 +60,7 @@ class FaissRetriever(BaseRetriever):
         Returns:
             _type_: retriever
         """
-
+        # print('_init_retriever', self.texts[0].metadata.keys())
         docsearch = FAISS.from_documents(self.texts, self.embeddings)
         
         retriever = docsearch.as_retriever(**kwargs)
@@ -64,8 +78,17 @@ class FaissRetriever(BaseRetriever):
         all_docs = []
         for query in batch_questions:
             docs = self.retriever.get_relevant_documents(query)
-            docs = [langchain_doc_to_open_rqa_doc(doc) for doc in docs]
-            all_docs.append(docs)
+            # convert back to ours
+            our_docs = []
+            for doc in docs:
+                our_doc = Document.from_langchain_doc(doc)
+                metadata = deepcopy(doc.metadata)
+                our_doc.page_content = metadata.pop('page_content', None)
+                our_doc.fmt_content = metadata.pop('fmt_content', None)
+                our_doc.metadata = metadata
+                our_docs.append(our_doc)
+            
+            all_docs.append(our_docs)
 
         output = RetrievalOutput(
             batch_source_documents=all_docs
--- open_rqa/trainers/retriever/model/wrappers.py
+++ open_rqa/trainers/retriever/model/wrappers.py
@@ -14,59 +14,61 @@ logger = logging.getLogger(__name__)
 
 
 class RetrievalModel(ABC):
-	embeddings = None
+    embeddings = None
 
-	def __init__(self, *args, **kwargs):
-		return
-	
-	@abstractmethod
-	def build_index(self, documents: List[Document] = [], format_str='title: {title} content: {text}') -> torch.Tensor:
-		raise NotImplementedError
-	
-	@abstractmethod
-	def retrieve(self, batch_inputs, documents=None, indices=None) -> RetrievalOutput:
-		raise NotImplementedError
+    def __init__(self, *args, **kwargs):
+        return
+    
+    @abstractmethod
+    def build_index(self, documents: List[Document] = [], format_str='title: {title} content: {text}') -> torch.Tensor:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def retrieve(self, batch_inputs, documents=None, indices=None) -> RetrievalOutput:
+        raise NotImplementedError
 
 
 class RetrieverfromBertModel(RetrievalModel):
-	def __init__(self, model, tokenizer, search_kwargs):
-		self.model = model
-		self.tokenizer = tokenizer
-		self.search_args = search_kwargs
-		self.embeddings: LocalEmbeddings = None
-		return
-	
-	def build_index(self, documents: List[Document] = [], format_str='title: {title} content: {text}') -> torch.Tensor:
-		embeddings = LocalEmbeddings(self.model, self.tokenizer, index_path=None)
+    def __init__(self, model, tokenizer, search_kwargs):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.search_args = search_kwargs
+        self.embeddings: LocalEmbeddings = None
+        return
+    
+    def build_index(self, documents: List[Document] = [], format_str='title: {title} content: {text}') -> torch.Tensor:
+        embeddings = LocalEmbeddings(self.model, self.tokenizer, index_path=None)
 
-		# asuming we are doing an inner product search, then normalize_L2=False
-		normalize_L2 = self.search_args['search_kwargs'].pop('normalize_L2', False)
-		docsearch = FAISS.from_documents(documents, embeddings)  # this will call from_texts under the hood
-		self.embeddings = embeddings
-		
-		# self.retriever = docsearch.as_retriever(**self.search_args)
-		self.retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={'k': 4})
-		embedding_tensor = torch.tensor(self.embeddings.document_embeddings)
-		return embedding_tensor
-	
-	def _get_docs(self, question: str, inputs: Dict[str, Any]) -> List[Document]:
-		docs = self.retriever.get_relevant_documents(question)
-		return docs
-	
-	def _batch_get_docs(self, questions: List[str], inputs: Dict[str, Any]) -> List[List[Document]]:
-		all_docs = []
-		for question in questions:
-			docs = self._get_docs(question, inputs)
-			all_docs.append(docs)
-		return all_docs
-	
-	def retrieve(self, batch_inputs, passages=None, indices=None) -> RetrievalOutput:
-		questions = batch_inputs["question"]
-		chat_history_strs = batch_inputs["chat_history_str"]
+        # asuming we are doing an inner product search, then normalize_L2=False
+        normalize_L2 = self.search_args['search_kwargs'].pop('normalize_L2', False)
+        docsearch = FAISS.from_documents(documents, embeddings)  # this will call from_texts under the hood
+        self.embeddings = embeddings
+        
+        # self.retriever = docsearch.as_retriever(**self.search_args)
+        self.retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={'k': 4})
+        embedding_tensor = torch.tensor(self.embeddings.document_embeddings)
+        return embedding_tensor
+    
+    def _get_docs(self, question: str, inputs: Dict[str, Any]) -> List[Document]:
+        docs = self.retriever.get_relevant_documents(question)
+        # convert langchain Doc to our Doc
+        docs = [Document.from_langchain_doc(doc) for doc in docs]
+        return docs
+    
+    def _batch_get_docs(self, questions: List[str], inputs: Dict[str, Any]) -> List[List[Document]]:
+        all_docs = []
+        for question in questions:
+            docs = self._get_docs(question, inputs)
+            all_docs.append(docs)
+        return all_docs
+    
+    def retrieve(self, batch_inputs, passages=None, indices=None) -> RetrievalOutput:
+        questions = batch_inputs["question"]
+        chat_history_strs = batch_inputs["chat_history_str"]
 
-		# retrieve
-		docs = self._batch_get_docs(questions, {})
-		output = RetrievalOutput(
+        # retrieve
+        docs = self._batch_get_docs(questions, {})
+        output = RetrievalOutput(
             batch_source_documents=docs
         )
-		return output
+        return output
