06e0d10ca988dbf5ff9082aabc23149892b5c32c
Yunan Lu <luyunan0404@gmail.com>
2024-03-05 17:16:53 -0500

    update quickstart pipeline

--- demo.py
+++ demo.py
@@ -1,12 +1,9 @@
-from langchain.document_loaders import *
-from langchain.text_splitter import *
-from langchain.embeddings import *
+from langchain_community.document_loaders import SeleniumURLLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import *
 from local_rqa.pipelines.retrieval_qa import SimpleRQA
 from local_rqa.schema.dialogue import DialogueSession
-from local_rqa.guardrails.base import NoopAnswerGuardrail
-from local_rqa.retrievers.faiss_retriever import FaissRetriever
 from local_rqa.text_loaders.langchain_text_loader import LangChainTextLoader
-from local_rqa.qa_llms.huggingface import HuggingFaceQAModel
 from local_rqa.utils import init_logger
 import logging
 import torch
@@ -15,79 +12,25 @@ import torch
 logger: logging.Logger
 
 
-def full():
-    ###### Manual usage of RQA ######
-    # a quick way to load data into retriever
-    # documents = DirectoryTextLoader("/local2/data/shared/rqa/data").load_data()
-
-    # Customized way by using different document_loaders provided in LangChain 
-    loader_func, splitter_func = DirectoryLoader, CharacterTextSplitter
-    loader_parameters = {'path': "/local2/data/shared/rqa/data", 'glob': "**/*.txt"}
-    splitter_parameters = {'chunk_size': 500, 'chunk_overlap': 200, 'separator': "\n\n"}
-    kwargs = {"loader_params": loader_parameters, "splitter_params": splitter_parameters}
-    documents = LangChainTextLoader(loader_func=loader_func, splitter_func=splitter_func).load_data(**kwargs)
-
-    # retriever: BaseRetriever = DummyRetriever()
-
-    # FaissRetriever by using different embeddings provided in Langchain, the embedding index will be cached for faster later use case
-    # retriever = FaissRetriever(
-    #     documents,
-    #     embeddings=OpenAIEmbeddings(
-    #         model='text-embedding-ada-002',
-    #         organization=os.environ['OPENAI_ORGANIZATION']
-    #     ),
-    #     index_path="/local2/data/shared/rqa/index"
-    # )
-
-    # Finetuned
-    retriever = FaissRetriever(
-        documents,
-        embeddings=HuggingFaceEmbeddings(
-            model_name="/local2/data/shared/rqa/retriever_model/contriever-ms_databricks_inbatch256_hard0.05_test/checkpoint-300"
-        ),
-        index_path="/local2/data/shared/rqa/index"
-    )
-
-    
-    # testcase
-    output = retriever.retrieve(batch_questions=['What are some changes in behavior in Databricks Runtime 8.0?'])
-    print(output.batch_source_documents)
-
-    # pick a QA model
-    qa_llm = HuggingFaceQAModel(model_name_or_path="lmsys/vicuna-13b-v1.5-16k")
-    # pick an answer guardrail
-
-    answer_guardrail = NoopAnswerGuardrail()
-
-    # QA!
-    rqa = SimpleRQA(retriever=retriever, qa_llm=qa_llm, answer_guardrail=answer_guardrail, verbose=True)
-
-    response = rqa.qa(
-        batch_questions=['what does Revvo do?'],
-        batch_dialogue_session=[DialogueSession()],
-    )
-    print(response.batch_answers[0])
-    return
-
-
 def quickstart():
-    qa_model_init_args = {
-        'torch_dtype': torch.float16,
-    }
+    loader_func, split_func = SeleniumURLLoader, CharacterTextSplitter
+    loader_parameters = {'urls': ["https://docs.databricks.com/en/dbfs/index.html"]}
+    splitter_parameters = {'chunk_size': 400, 'chunk_overlap': 50, 'separator': "\n\n"}
+    kwargs = {"loader_params": loader_parameters, "splitter_params": splitter_parameters}
+    docs = LangChainTextLoader(save_folder="./result/demo", save_filename="databricks_web", loader_func=loader_func, splitter_func=split_func).load_data(**kwargs)
+
     rqa = SimpleRQA.from_scratch(
-        document_path="data/database/databricks/databricks_400.pkl",
-        index_path="data/database/databricks/databricks_400_e5-base-v2",
+        document_path="./result/demo/databricks_web.pkl",
+        index_path="./result/demo/index",
         embedding_model_name_or_path="intfloat/e5-base-v2",
-        qa_model_name_or_path="lmsys/vicuna-7b-v1.5",
-        qa_model_init_kwargs=qa_model_init_args,
-        qa_is_fid=False,
-        verbose=True,
+        qa_model_name_or_path="lmsys/vicuna-7b-v1.5"
     )
     response = rqa.qa(
         batch_questions=['What is DBFS?'],
         batch_dialogue_session=[DialogueSession()],
     )
-    print(response.batch_answers[0])
+    # print(response.batch_answers[0])
+    print(response)
     return
 
 
@@ -97,9 +40,6 @@ def quickstart_vllm():
         index_path="data/database/databricks/databricks_400_e5-base-v2",
         embedding_model_name_or_path="intfloat/e5-base-v2",
         qa_model_name_or_path="vllm::http://localhost:8000/generate",  # where 'http://localhost:8000/generate' is the address of the vllm server
-        qa_model_init_kwargs={},
-        qa_is_fid=False,
-        verbose=True,
     )
     response = rqa.qa(
         batch_questions=['What is DBFS?'],
@@ -109,25 +49,6 @@ def quickstart_vllm():
     return
 
 
-def auto():
-    # ##### Auto usage of RQA ######
-    # documents = SimpleDirectoryReader("data").load_data()
-    # rqa = AutoRQA(
-    #     documents,
-    #     retriever="e5",
-    #     qa_llm="llama-2",
-    # )
-
-    # response = rqa.qa(
-    #     batch_questions=["What is the capital of the United States?"],
-    #     batch_dialogue_session=[DialogueSession()],
-    # )
-    # print(response)
-    return
-
 if __name__ == "__main__":
     logger = init_logger()
-
-    # full()
     quickstart()
-    auto()
--- docs/getting_started/quickstart.rst
+++ docs/getting_started/quickstart.rst
@@ -6,6 +6,8 @@ In this quickstart we'll show you how to:
 - How to prepare documents for retrieval
 - How to use ``SimpleRQA`` to get the response based on the existing embedding model and generative model
 
+You could also run ``quickstart()`` function in ``demo.py`` script to directly start the pipeline.
+
 
 Prepare Document
 ----------------
@@ -14,15 +16,15 @@ LocalRQA supports integration with frameworks such as LangChain and LlamaIndex t
 
 For example, you could prepare document from website url
 ::
-    from langchain.document_loaders import *
-    from langchain.text_splitter import *
+    from langchain_community.document_loaders import SeleniumURLLoader
+    from langchain.text_splitter import CharacterTextSplitter
     from local_rqa.text_loaders.langchain_text_loader import LangChainTextLoader
 
     loader_func, split_func = SeleniumURLLoader, CharacterTextSplitter
     loader_parameters = {'urls': ["<website_url>"]}
     splitter_parameters = {'chunk_size': 400, 'chunk_overlap': 50, 'separator': "\n\n"}
     kwargs = {"loader_params": loader_parameters, "splitter_params": splitter_parameters}
-    docs = LangChainTextLoader(save_folder="<path/to/save/dir>", save_filename="<filename>", loader_func, split_func).load_data(**kwargs)
+    docs = LangChainTextLoader(save_folder="<path/to/save/dir>", save_filename="<filename>", loader_func=loader_func, splitter_func=split_func).load_data(**kwargs)
 
 
 Pipeline
@@ -31,15 +33,21 @@ By providing document that generated in the previous step, and specifying the ``
 ::
     rqa = SimpleRQA.from_scratch(
         document_path="path/to/save/dir/filename",
+        index_path="path/to/index",
         embedding_model_name_or_path="intfloat/e5-base-v2",
-        qa_model_name_or_path="lmsys/vicuna-7b-v1.5",
-        qa_model_init_kwargs=qa_model_init_args,
-        qa_is_fid=False,
-        verbose=True,
+        qa_model_name_or_path="lmsys/vicuna-7b-v1.5"
     )
     response = rqa.qa(
         batch_questions=['What is DBFS?'],
         batch_dialogue_session=[DialogueSession()],
     )
     final_answer = response.batch_answers[0]
-where ``final_answer`` is the response after post process.
+
+where ``response`` is a ``RQAOutput`` object with the following construction::
+
+    class RQAOutput:
+        """stores the answers to a user's question, the relevant source documents, and the UPDATED dialogue history"""
+        batch_answers: List[str]
+        batch_source_documents: List[List[Document]]
+        batch_dialogue_session: List[DialogueSession]
+
