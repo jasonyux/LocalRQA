c4d7f64e76970e11734ba52099fdda525f9050c2
Yunan Lu <luyunan0404@gmail.com>
2024-03-16 15:43:52 -0400

    clean demo code

--- demo.py
+++ demo.py
@@ -1,12 +1,10 @@
+import logging
 from langchain_community.document_loaders import SeleniumURLLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings import *
 from local_rqa.pipelines.retrieval_qa import SimpleRQA
 from local_rqa.schema.dialogue import DialogueSession
 from local_rqa.text_loaders.langchain_text_loader import LangChainTextLoader
 from local_rqa.utils import init_logger
-import logging
-import torch
 
 
 logger: logging.Logger
@@ -17,10 +15,15 @@ def quickstart():
     loader_parameters = {'urls': ["https://docs.databricks.com/en/dbfs/index.html"]}
     splitter_parameters = {'chunk_size': 400, 'chunk_overlap': 50, 'separator': "\n\n"}
     kwargs = {"loader_params": loader_parameters, "splitter_params": splitter_parameters}
-    docs = LangChainTextLoader(save_folder="./result/demo", save_filename="databricks_web", loader_func=loader_func, splitter_func=split_func).load_data(**kwargs)
+    docs = LangChainTextLoader(
+        save_folder="./result/demo/databricks", 
+        save_filename="databricks_web", 
+        loader_func=loader_func, 
+        splitter_func=split_func
+    ).load_data(**kwargs)
 
     rqa = SimpleRQA.from_scratch(
-        document_path="./result/demo/databricks_web.pkl",
+        document_path="./result/demo/databricks/databricks_web.pkl",
         index_path="./result/demo/index",
         embedding_model_name_or_path="intfloat/e5-base-v2",
         qa_model_name_or_path="lmsys/vicuna-7b-v1.5"
@@ -29,8 +32,7 @@ def quickstart():
         batch_questions=['What is DBFS?'],
         batch_dialogue_session=[DialogueSession()],
     )
-    # print(response.batch_answers[0])
-    print(response)
+    print(response.batch_answers[0])
     return
 
 
